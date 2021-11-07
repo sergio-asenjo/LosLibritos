3717aac90b272712a0e0b95d79203778301a07d9
Sergio Asenjo <sergio.asenjo@usach.cl>
2021-11-07 18:40:56 -0300

    cambios en dto, añadida herencia en negocio

--- CapaDTO/Usuario.cs
+++ CapaDTO/Usuario.cs
@@ -1,30 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CapaDTO
 {
     public class Usuario
     {
-        private int id_usuario;
-        private String nombre;
-        private String apaterno;
-        private String amaterno;
-        private String rut;
-        private String fecha_nacimiento;
-        private String username;
-        private String contrasena;
-        private String email;
-        private String direccion;
-        private String ciudad;
-        private String numero_telefono;
-        private String fecha_creacion;
-        private int id_empleado;
-        private int id_cliente;
+        protected String nombre;
+        protected String apaterno;
+        protected String amaterno;
+        protected String rut;
+        protected String fecha_nacimiento;
+        protected String username;
+        protected String contrasena;
+        protected String email;
+        protected String direccion;
+        protected String ciudad;
+        protected int numero_telefono;
+        protected String fecha_creacion;
 
-        public int Id_usuario { get => id_usuario; set => id_usuario = value; }
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apaterno { get => apaterno; set => apaterno = value; }
         public string Amaterno { get => amaterno; set => amaterno = value; }
@@ -35,9 +27,7 @@ namespace CapaDTO
         public string Email { get => email; set => email = value; }
         public string Direccion { get => direccion; set => direccion = value; }
         public string Ciudad { get => ciudad; set => ciudad = value; }
-        public string Numero_telefono { get => numero_telefono; set => numero_telefono = value; }
+        public int Numero_telefono { get => numero_telefono; set => numero_telefono = value; }
         public string Fecha_creacion { get => fecha_creacion; set => fecha_creacion = value; }
-        public int Id_empleado { get => id_empleado; set => id_empleado = value; }
-        public int Id_cliente { get => id_cliente; set => id_cliente = value; }
     }
 }
--- CapaNegocio/Negocio.cs
+++ CapaNegocio/Negocio.cs
@@ -1,27 +1,21 @@
-﻿using CapaDTO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CapaConexion;
 
 namespace CapaNegocio
 {
     public class Negocio
     {
-        private Conexion conec1;
-        public Conexion Conec1 { get => conec1; set => conec1 = value; }
-        public void configurarConexion()
-        {
-            this.Conec1 = new Conexion();
-            this.Conec1.NombreBaseDeDatos = "prueba";
-            this.Conec1.NombreTabla = "transaccion";
-            this.Conec1.CadenaConexion = "Data Source=localhost;Initial Catalog=Prueba;Integrated Security=True";
-        }
-        public void guardarPrestamo(Prestamo prestamo)
-        {
-            this.configurarConexion();
+        protected Conexion conec1;
 
+        public Conexion Conec1 { get => conec1; set => conec1 = value; }
+
+        public void ConfigurarConexion(string nombreTabla)
+        {
+            this.Conec1 = new Conexion
+            {
+                NombreBaseDeDatos = "loslibritos",
+                NombreTabla = nombreTabla,
+                CadenaConexion = "Data Source=localhost;Initial Catalog=loslibritos;Integrated Security=True"
+            };
         }
     }
 }
